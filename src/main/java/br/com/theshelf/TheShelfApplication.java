bc5a955cd34f7603df0a2f24e2e3a611acf6db56
Juan Gomes <juangsantanna@gmail.com>
2022-11-15 23:58:26 -0300

    first commit

---
+++ src/main/java/br/com/theshelf/TheShelfApplication.java
@@ -0,0 +1,13 @@
+package br.com.theshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/theshelf/TheShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.theshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
